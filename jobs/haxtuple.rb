a8b84a14cd99883294f0b3de3581652174af2e41
Linda Nichols <lynnaloo@gmail.com>
2014-08-14 20:46:15 +0000

    rebuild

--- jobs/haxtuple.rb
+++ jobs/haxtuple.rb
@@ -1,12 +1,14 @@
 
 SCHEDULER.every '5m' do
 
+  # Bug fixers
   tuplers = [
     {:label=>"Jeff Gunderson", :value=>1},
     {:label=>"Steve Hackbarth", :value=>1},
     {:label=>"Mark Gummo", :value=>1}
   ]
 
+  # Extension builders
   extensions = [
     {:label=>"Juliana Louback", :value=>"JS Communicator", :url=>"https://github.com/xtuple/xtuple/issues/1702"},
     {:label=>"Stanley Zheng", :value=>"xTuple-Yo", :url=>"https://github.com/stanzheng/xTuple-Yo"},
