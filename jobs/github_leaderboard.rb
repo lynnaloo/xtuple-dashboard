f404f83fd6588d051fe9607d2241a20c1606d510
Linda Nichols <lynnaloo@gmail.com>
2014-08-15 08:20:01 +0000

    added sorting for leaderboard

--- jobs/github_leaderboard.rb
+++ jobs/github_leaderboard.rb
@@ -12,13 +12,10 @@ org    = ENV['GITHUB_ORG']
 token  = ENV['GITHUB_TOKEN']
 label  = 'haxtuple'
 
-## Change this if you want to run more than one set of issue widgets
-event_name = "git_issues_labeled_defects"
-
 ## the endpoint we'll be hitting
 uri = "https://api.github.com/orgs/#{org}/issues?state=closed&filter=all&labels=#{label}&access_token=#{token}"
 
-SCHEDULER.every '5m', :first_in => 0 do |job|
+SCHEDULER.every '1m', :first_in => 0 do |job|
     response = RestClient.get uri
     issues = JSON.parse(response.body)
     issue_counts = Hash.new({ value: 0 })
@@ -30,11 +27,16 @@ SCHEDULER.every '5m', :first_in => 0 do |job|
           person = issue['user']
           login = person['login']
           avatar = person['avatar_url']
-          issue_counts[login] = { label: login, avatar_url: avatar, value: issue_counts[person['login']][:value] + 1}
+          issue_counts[login] = { label: login, avatar_url: avatar,
+            value: issue_counts[person['login']][:value] + 1}
         end
       end
     end
 
-    send_event('leaderboard', { items: issue_counts.values })
+    # sort by value
+    sorted = issue_counts.values.sort_by { |hsh| -hsh[:value] }
+
+    # todo - sort these
+    send_event('leaderboard', { items: sorted })
 
 end # SCHEDULER
